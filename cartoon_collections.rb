e04ad4d1ed4519eff32365aaec9cc5d6e6d322df
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-25 19:44:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(words)
   words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese
+def find_the_cheese(food_list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
