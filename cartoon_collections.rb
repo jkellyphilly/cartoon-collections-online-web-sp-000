205681996b4e6d2c34d1519d5cb6d86ed158247e
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-25 19:40:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index {|dwarf, index| puts "#{index + 1} #{dwarf}"}
 end
 
-def summon_captain_planet
-  
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect {|call| }
 end
 
 def long_planeteer_calls# code an argument here
